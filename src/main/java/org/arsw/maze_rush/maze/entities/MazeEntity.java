7c81818dd050e060c6bde936eb4e779644b919e0
Jeisson Sanchez <jeisson.sanchez-g@mail.escuelaing.edu.co>
2025-11-20 23:05:39 -0500

    Agregar utilidades para manipular layout y definir codigos de power ups #420 in progress

--- src/main/java/org/arsw/maze_rush/maze/entities/MazeEntity.java
+++ src/main/java/org/arsw/maze_rush/maze/entities/MazeEntity.java
@@ -5,6 +5,9 @@ import lombok.*;
 
 import java.util.UUID;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 @Entity
 @Table(name = "mazes")
 @Getter
@@ -42,4 +45,44 @@ public class MazeEntity {
 
     @Column(nullable = false)
     private int goalY;
+
+    public static final int PU_SPEED = 2;
+    public static final int PU_SCORE = 3;
+    public static final int PU_LIFE = 4;
+
+    public static final int WALL = 1;
+    public static final int EMPTY = 0;
+
+    public static final int[] POWER_UP_TYPES = { PU_SPEED, PU_SCORE, PU_LIFE };
+
+    public boolean placePowerUp(int x, int y, int type) {
+        int[][] matrix = getLayoutMatrix();
+
+        if (matrix[x][y] == EMPTY) {
+            matrix[x][y] = type;
+            setLayoutMatrix(matrix);
+            return true;
+        }
+
+        return false;
+    }
+
+
+    @Transient
+    private static final ObjectMapper mapper = new ObjectMapper();
+    public int[][] getLayoutMatrix() {
+        try {
+            return mapper.readValue(this.layout, int[][].class);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("Error parsing maze layout JSON", e);
+        }
+    }
+
+    public void setLayoutMatrix(int[][] matrix) {
+        try {
+            this.layout = mapper.writeValueAsString(matrix);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("Error writing layout JSON", e);
+        }
+    }
 }
